31db181250002b5d55a1ec88f39cfffede0fb5c4
swiftcodekeys <your-email@example.com>
2026-04-17 19:47:56 -0400

    chore(router): enable v7_startTransition + v7_relativeSplatPath future flags

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
